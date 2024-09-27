919e4b4cd6b9e2095c4dfcff7d0f91d2dde5d3dc
Vitor Hugo <vitor_hln@outlook.com>
2024-09-27 00:04:07 -0300

    Alterações no codigo inicial

--- BOT.py
+++ BOT.py
@@ -2,7 +2,13 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv 
 import os
 
-load_dotenv()
-llm = ChatOpenAI(api_key=os.getenv("KEY"))
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo-1106",
+    temperature=0.2,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    api_key=os.getenv("KEY"),
+    )
 
 llm.invoke("how can langsmith help with testing?")
